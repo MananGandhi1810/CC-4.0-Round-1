b54806390f0b84fa64b6668cf4c08b05f7789549
Manan Gandhi <ardumanan@gmail.com>
2025-02-15 19:27:57 +0530

    Hero section

--- frontend/src/components/custom/Hero.jsx
+++ frontend/src/components/custom/Hero.jsx
@@ -2,15 +2,14 @@ import { Globe } from "@/components/ui/globe";
 
 export function Hero() {
     return (
-        <div className="relative flex items-center justify-center overflow-hidden rounded-lg border bg-background px-40 pb-40 pt-8 md:pb-60 md:shadow-xl">
-            <div className="flex flex-col items-center">
-                <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+        <div className="relative flex items-center justify-center overflow-hidden rounded-lg bg-background px-40 pb-40 pt-8 md:pb-60 md:shadow-xl h-full-w-nav">
+            <div className="flex flex-col items-center text-gray-500">
+                <span className="pointer-events-none whitespace-pre-wrap bg-clip-text text-center text-8xl font-semibold leading-none z-10">
                     Dream Scale
                 </span>
-                <span>Scale Your Startup Beyond Your Dreams</span>
+                <span className="text-4xl z-10 text-center">Scale Your Startup Beyond Your Dreams</span>
             </div>
-            <Globe className="top-28" />
-            <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+            <Globe className="md:scale-100 scale-75" />
         </div>
     );
 }
