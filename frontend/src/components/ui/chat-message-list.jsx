83c1b5a8796618b4360c2eff82d7f6aa64e1b2b4
Manan Gandhi <ardumanan@gmail.com>
2025-02-16 12:09:21 +0530

    [all] Design changes, remove duplicate folder

--- frontend/src/components/ui/chat-message-list.jsx
+++ frontend/src/components/ui/chat-message-list.jsx
@@ -1,16 +1,17 @@
 import * as React from "react";
+import { ScrollArea } from "./scroll-area";
 
 const ChatMessageList = React.forwardRef(
     ({ className, children, smooth = false, ...props }, _ref) => {
         return (
-            <div className="relative w-full h-full">
+            <ScrollArea className="relative w-full h-full">
                 <div
                     className={`flex flex-col w-full h-full p-4 overflow-y-auto ${className}`}
                     {...props}
                 >
                     <div className="flex flex-col gap-6">{children}</div>
                 </div>
-            </div>
+            </ScrollArea>
         );
     },
 );
